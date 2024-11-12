812b8835672ad1a74a32a58b3600beca82945806
Jennifer Lucifero <jennifer.lucifero@codecentric.de>
2024-11-12 12:18:15 +0100

    fetch user info

--- src/app/components/TaskListComponent.tsx
+++ src/app/components/TaskListComponent.tsx
@@ -7,12 +7,32 @@ interface Task {
     completed: false
 }
 
-export const TaskListComponent = async ({userId}: { userId: number }) => {
+async function fetchTasksFor(userId: number) {
     const result = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}/todos`);
     const tasks: Task[] = await result.json();
+    return tasks;
+}
+
+interface User {
+    username: string,
+    name: string
+}
+
+async function fetchUserInfoFor(userId: number) {
+    const result = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
+    const user: User = await result.json();
+    return user;
+}
+
+export const TaskListComponent = async ({userId}: { userId: number }) => {
+    const tasks = await fetchTasksFor(userId);
+    const user = await fetchUserInfoFor(userId);
+
+    console.log(user);
 
     return (
         <div className="container container-sm p-5">
+            <p className="py-3">Aufgaben von {user.name}</p>
             <table className="table table-bordered">
                 <thead>
                 <tr>
